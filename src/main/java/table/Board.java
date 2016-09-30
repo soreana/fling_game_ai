a0502a5e6ae6364d8de4f8d075e1fc90190882e6
Sina kashi Pazha <esterlinkof@gmail.com>
2016-09-30 19:27:48 +0330

    Client of class Board can check their board for these conditions: failed, solved and canMove.

--- src/main/java/table/Board.java
+++ src/main/java/table/Board.java
@@ -11,13 +11,14 @@ import java.util.Scanner;
  * Created by sinakashipazha on 9/30/16.
  */
 abstract public class Board {
-    private class Node {
+    private int nodeCount =0;
 
+    private class Node {
     }
 
     private ArrayList<ArrayList<Node>> board;
 
-    Board(){
+    Board() {
         board = new ArrayList<>();
         File fling = new File("target/fling.txt");
         Scanner inputFile = null;
@@ -31,31 +32,33 @@ abstract public class Board {
         inputBoard(inputFile);
     }
 
-    private void inputBoard(Scanner inputFile){
-        while (inputFile.hasNext()){
+    private void inputBoard(Scanner inputFile) {
+        while (inputFile.hasNext()) {
             board.add(putALineToBoard(inputFile.nextLine()));
         }
     }
 
-    private ArrayList<Node> putALineToBoard(String line){
+    private ArrayList<Node> putALineToBoard(String line) {
         ArrayList<Node> temp = new ArrayList<>();
 
-        for(int i=0; i< line.length() ; i++) {
-            if(line.charAt(i) == '0')
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '0')
                 temp.add(null);
-            else if (line.charAt(i) == '1')
+            else if (line.charAt(i) == '1') {
                 temp.add(new Node());
+                nodeCount++;
+            }
         }
 
         return temp;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String result = "";
         int tableWidth = board.get(0).size();
 
-        for (int i=0 ; i<tableWidth; i++)
+        for (int i = 0; i < tableWidth; i++)
             result += "__";
         result += "_\n";
 
@@ -74,11 +77,59 @@ abstract public class Board {
     }
 
     /**
-     * Implement this method based on your choice
+     *  Check if there is a node in board that can moves.
+     *
+     * @return true if board already solved, false otherwise.
+     */
+
+    public boolean solved() {
+        return !canMove() && nodeCount <= 1;
+    }
+
+    // TODO implement this method
+
+    public boolean canMove(Node node) {
+        return false;
+    }
+
+
+    /**
+     * Start from (0,0) and check each node to see it can moves or not.
+     * If it can moves terminates search and return true, if it can not moves,
+     * check next node;
+     *
+     * @return true if there is a node that can moves
+     */
+
+    public boolean canMove(){
+        for (ArrayList<Node> line : board){
+            for( Node node : line){
+                if (node == null) continue;
+                if ( canMove(node))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * if board can't be solved return true, false otherwise.
+     *
+     * @return true if board failed, false otherwise
+     */
+
+    public boolean failed(){
+        return !solved() && !canMove();
+    }
+
+
+    /**
+     * Implement this method based on your preferred algorithm
      *
      * @param algorithmName name of solver algorithm
      * @return algorithm you want to solve
      */
 
-    public abstract Algorithm getSolvingAlgorithm (String algorithmName );
+    public abstract Algorithm getSolvingAlgorithm(String algorithmName);
 }
