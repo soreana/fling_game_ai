1c3925489e8985726a3e40f3d74f1f9b3e0a4757
Sina kashi Pazha <esterlinkof@gmail.com>
2016-10-02 05:32:22 +0330

    remove unused method from BoardIterator.

--- src/main/java/table/BoardIterator.java
+++ src/main/java/table/BoardIterator.java
@@ -4,7 +4,7 @@ package table;
  * Created by sinakashipazha on 10/2/16.
  */
 public interface BoardIterator {
+    Ball current();
     Ball next();
     Ball previous();
-    boolean currentNodeEquals(Board.Node node);
 }
